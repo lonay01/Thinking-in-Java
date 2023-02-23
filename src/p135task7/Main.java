dfd1b085498899498479e48093a5be5bbe9e6762
Сережа <shustov.sa@phystech.edu>
2023-02-23 12:55:50 +0300

    3

---
+++ src/p135task7/Main.java
@@ -0,0 +1,11 @@
+package p135task7;
+
+public class Main {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 100; i++) {
+            System.out.println(i);
+            if (i == 99) return;
+            // if (i == 99) break;
+        }
+    }
+}
