29af7557346841df0206507d0857c66fd7af7fbe
Сережа <shustov.sa@phystech.edu>
2023-08-06 13:01:31 +0300

    ez

---
+++ src/p281task17/Main.java
@@ -0,0 +1,11 @@
+package p281task17;
+
+interface A {
+    int i = 1;
+}
+public class Main implements A{
+    public static void main(String[] args) {
+        //A.i = 2;
+        System.out.println(i);
+    }
+}
